d73454206c7a37a6d25db559e661438c92be0bed
zhangyi <2369717781@qq.com>
2018-02-28 22:29:51 +0800

    test

--- narutocity_backend/settings.py
+++ narutocity_backend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-
+os.environ['DJANGO_SETTINGS_MODULE'] = 'narutocity_backend.settings'
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
